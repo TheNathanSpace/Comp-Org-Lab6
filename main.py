ad8f7a0ae9f615708f1f4e7f16a5782fd1e587d7
TheNathanSpace <wasabimath@gmail.com>
2022-11-26 15:10:55 -0700

    Reading config file

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import json
+import re
+from pathlib import Path
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def read_config_file(config_file: Path):
+    """
+    Reads the config.txt file and returns a dict of the values
+    :param config_file: The Path object of the config.txt file
+    :return: A dict of the config values
+    """
+    config_dict = {}
+    with open(file = config_file, mode = "r", encoding = "utf-8") as opened:
+        lines = opened.readlines()
+        for line in lines:
+            # Comment line
+            if re.match(string = line, pattern = " *#.*"):
+                continue
+            # Blank line
+            elif re.match(string = line, pattern = "^ *$"):
+                continue
+            # Config line
+            elif re.match(string = line, pattern = "^ *([^ ]*) *= *(\d*) *$"):
+                matched = re.match(string = line, pattern = "^ *([^ ]*) *= *(\d*) *$")
+                config_dict[matched.group(1)] = int(matched.group(2))
+            # Assume everything else is an error
+            else:
+                line_stripped = line.replace("\n", "")
+                print(f"ERROR: Invalid line in config.txt:\n{line_stripped}\nExiting.")
+                exit(-1)
+    return config_dict
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Load config
+    config_file = Path("config.txt")
+    if not config_file.exists():
+        print("ERROR: config.txt does not exist. Exiting.")
+        exit(-1)
+    config_dict = read_config_file(config_file = config_file)
+    print(json.dumps(config_dict, indent = 4))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
